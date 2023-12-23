c9fb358ad93c7e0565f6bf3570bc3619936f2c59
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2023-12-22 22:53:03 -0500

    Changing Produc-routes file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -124,14 +124,8 @@ router.delete('/:id', (req, res) => {
   Product.destroy({
     where: {id:req.params.id}
   })
-  .then((products) => {
-    console.log(products);
-    res.json(products);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err); 
+  .then((product) => res.status(200).json(product))
+  .catch(( err) => res.status(400).json(err));
   });
-});
 
 module.exports = router;
